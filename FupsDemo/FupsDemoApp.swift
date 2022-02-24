0a536f10281b39083c532a21e8ef91e82d976405
GokhanMutlu <gokhanmutlu04@yahoo.com>
2022-02-24 08:33:00 +0300

    Initial Commit

---
+++ FupsDemo/FupsDemoApp.swift
@@ -0,0 +1,12 @@
+// FupsDemoApp.swift by Gokhan Mutlu on 24.02.2022
+
+import SwiftUI
+
+@main
+struct FupsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
